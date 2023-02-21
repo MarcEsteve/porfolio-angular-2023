c77163cba26c91ec9900ae6d1d133732bdd49549
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-02-21 14:46:57 +0100

    más

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,12 +8,25 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false; 
+  equipo: any = [];
   constructor( private http:HttpClient ) {
-    // console.log('Servicio de infoPagina listo');
+    this.cargaInfo();
+    this.cargarEquipo();
+  }
+  private cargaInfo(){
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp) => {
       this.cargada = true;
       this.info = resp; 
+      // console.log(resp);
+      });
+  }
+
+  private cargarEquipo(){
+    this.http
+    .get('https://db-storage-web-angular-2023-default-rtdb.europe-west1.firebasedatabase.app/equip.json')
+    .subscribe( (resp) => {
+      this.equipo = resp; 
       console.log(resp);
       });
   }
